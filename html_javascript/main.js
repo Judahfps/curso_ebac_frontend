526f9b358173673c32700166e84b6881a8e42aba
Guilherme Silva <sebastian_49@hotmail.com>
2024-04-06 17:42:35 -0400

    ajuste no js, tinha sumido as verificações dos inputs

--- html_javascript/main.js
+++ html_javascript/main.js
@@ -3,43 +3,45 @@ const nome = [];
 const tel = [];
 const email = [];
 let linhas = '';
-
-
-
-
 formWrapper.addEventListener('submit', function(e) {
     e.preventDefault()
 
+    
     addContato();
     atualizarListaDeContatos()
 } )
-
 function addContato() {
-
     const inputNome = document.getElementById('input-nome');
     const inputTel = document.getElementById('input-tel');
     const inputEmail = document.getElementById('input-email');
 
-    nome.push(inputNome.value);
-    tel.push(parseInt(inputTel.value));
-    email.push(inputEmail);
+    if (nome.includes(inputNome.value)) {
+        alert(`O nome ${inputNome.value} já existe em seus contatos`)
+    }else if (tel.includes(parseInt(inputTel.value))) {
+        alert(`O número ${inputTel.value} já existe em seus contatos`)
+    }else if (email.includes(inputEmail.value)) {
+        alert(`O email ${inputEmail.value} já existe em seus contatos`)
+    }else {
 
-    let linha = '<tr>';
-    linha += `<td>${inputNome.value}</td>`;
-    linha += `<td>${inputTel.value}</td>`;
-    linha += `<td>${inputEmail.value}</td>`
+        nome.push(inputNome.value);
+        tel.push(parseInt(inputTel.value));
+        email.push(inputEmail.value);
 
-    linhas += linha
+        let linha = '<tr>';
+        linha += `<td>${inputNome.value}</td>`;
+        linha += `<td>${parseInt(inputTel.value)}</td>`;
+        linha += `<td>${inputEmail.value}</td>`
+        linha += `</tr>`
 
+        linhas += linha
+    }
 
     inputNome.value = '';
     inputTel.value = '';
     inputEmail.value = '';
 };
 
-
-
 function atualizarListaDeContatos() {
     const corpoTabela = document.querySelector('tbody');
     corpoTabela.innerHTML = linhas
-}
+};
