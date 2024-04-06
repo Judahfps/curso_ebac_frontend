b289e5bdab0b9ee69e138202c15eef9f314c4d36
Guilherme Silva <sebastian_49@hotmail.com>
2024-04-06 17:01:21 -0400

    ajustes na pasta do projeto

---
+++ html_javascript/main.js
@@ -0,0 +1,45 @@
+const formWrapper = document.getElementById('form-wrapper');
+const nome = [];
+const tel = [];
+const email = [];
+let linhas = '';
+
+
+
+
+formWrapper.addEventListener('submit', function(e) {
+    e.preventDefault()
+
+    addContato();
+    atualizarListaDeContatos()
+} )
+
+function addContato() {
+
+    const inputNome = document.getElementById('input-nome');
+    const inputTel = document.getElementById('input-tel');
+    const inputEmail = document.getElementById('input-email');
+
+    nome.push(inputNome.value);
+    tel.push(parseInt(inputTel.value));
+    email.push(inputEmail);
+
+    let linha = '<tr>';
+    linha += `<td>${inputNome.value}</td>`;
+    linha += `<td>${inputTel.value}</td>`;
+    linha += `<td>${inputEmail.value}</td>`
+
+    linhas += linha
+
+
+    inputNome.value = '';
+    inputTel.value = '';
+    inputEmail.value = '';
+};
+
+
+
+function atualizarListaDeContatos() {
+    const corpoTabela = document.querySelector('tbody');
+    corpoTabela.innerHTML = linhas
+}
