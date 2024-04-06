613b29f4a5455c6402ad0d8f7079ec76c9973eba
Guilherme Silva <sebastian_49@hotmail.com>
2024-04-06 16:39:02 -0400

    ajuste na script Js na let += linha, faltou colocar o .value

--- html_javascript/src/main.js
+++ html_javascript/src/main.js
@@ -17,25 +17,35 @@ function addContato() {
     const inputTel = document.getElementById('input-tel');
     const inputEmail = document.getElementById('input-email');
 
+    if (nome.includes(inputNome.value)) {
+        alert(`O nome ${inputNome.value} já existe em seus contatos`)
+    }else if (tel.includes(parseInt(inputTel.value))) {
+        alert(`O número ${inputTel.value} já existe em seus contatos`)
+    }else if (email.includes(inputEmail.value)) {
+        alert(`O email ${inputEmail.value} já existe em seus contatos`)
+    }else {
+         
         nome.push(inputNome.value);
         tel.push(parseInt(inputTel.value));
-        email.push(inputEmail);
+        email.push(inputEmail.value);
 
         let linha = '<tr>';
         linha += `<td>${inputNome.value}</td>`;
-        linha += `<td>${inputTel.value}</td>`;
+        linha += `<td>${parseInt(inputTel.value)}</td>`;
         linha += `<td>${inputEmail.value}</td>`
         linha += `</tr>`
 
         linhas += linha
+    }
 
 
-        inputNome.value = '';
-        inputTel.value = '';
-        inputEmail.value = '';
+    inputNome.value = '';
+    inputTel.value = '';
+    inputEmail.value = '';
 };
 
 function atualizarListaDeContatos() {
     const corpoTabela = document.querySelector('tbody');
     corpoTabela.innerHTML = linhas
 };
+
